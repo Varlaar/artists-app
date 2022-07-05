fbc2253c9f6c33790fa80270fb499b66a00a9a99
Varlaar <igorbjj73@mail.ru>
2022-07-06 00:23:38 +0400

    Changed the folder structure

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    return(
+        <div>
+            <img src="../assets/img/logo.png" alt="logo" />
+            <img src="../assets/img/switch_theme.png" alt="switch-theme" />
+        </div>
+    );
+}
+
+export default Header;
