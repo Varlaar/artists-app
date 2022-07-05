cff9d5d812480e9a494ff44c3dc2df1b20499d79
Varlaar <igorbjj73@mail.ru>
2022-07-06 01:03:07 +0400

    header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
+import "./App.css";
 
 function App() {
   return (
+    <div className="wrapper">
     <div className="App">
       <Header />
+    </div>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,12 @@
-
+import logo from "../assets/img/logo.png";
+import switch_theme from "../assets/img/switch_theme.png"
+import "./Header.css"
 
 function Header() {
     return(
-        <div>
-            <img src="../assets/img/logo.png" alt="logo" />
-            <img src="../assets/img/switch_theme.png" alt="switch-theme" />
+        <div className="container">
+            <img src={logo} alt="logo" />
+            <img src={switch_theme} alt="switch_theme" />
         </div>
     );
 }
