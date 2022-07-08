57ba849fadb369f50917d970a168d92e1ff1d14b
Varlaar <igorbjj73@mail.ru>
2022-07-09 01:28:36 +0400

    Fixed the component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header/Header';
 import './App.css';
 import Card from './components/Card';
+import { pictureCard } from './components/Card';
 
 function App() {
   return (
     <div className='wrapper'>
       <Header />
-      <Card />
+      <div className="card__wrapper">
+      {pictureCard.map(item => <Card key={item.title} card={item}/>)}
+      </div>
+      {/* <Card /> */}
     </div>
   );
 }
