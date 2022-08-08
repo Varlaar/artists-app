fa73789f18b98ede49e2bb7d12af23315d8d4ca8
Varlaar <igorbjj73@mail.ru>
2022-08-08 13:37:56 +0400

    switch_theme

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,23 @@
-import React from "react";
 import logo from "../assets/img/logo.png";
 import switch_theme from "../assets/img/switch_theme.png";
 import switch_theme_dark from "../assets/img/switch_theme_dark.png";
 import "./Header.css";
 
-function Header({onClick, isDarkTheme}) {
-   const changeTheme = (isDarkTheme) => {
-    return {isDarkTheme} ? switch_theme : switch_theme_dark
-   }
-    return(
-        <div className="container">
-            <img src={logo} alt="logo" />
-            <img onClick={onClick} src={changeTheme()} alt="switch_theme" />
-        </div>
-    );
+function Header({ onClick, isDarkTheme }) {
+  return (
+    <div className="container">
+      <img src={logo} alt="logo" />
+      {isDarkTheme ? (
+        <img
+          onClick={onClick}
+          src={switch_theme_dark}
+          alt="switch_theme_dark"
+        />
+      ) : (
+        <img onClick={onClick} src={switch_theme} alt="switch_theme" />
+      )}
+    </div>
+  );
 }
 
 export default Header;
